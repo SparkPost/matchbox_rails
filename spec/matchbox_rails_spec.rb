6950a90a3df747b1d6bde8da447e7d624575171d
AM <alejandro.marco@taxiforemail.com>
2022-01-28 11:50:10 +0000

    :white_check_mark: delete test and add TODO

--- spec/matchbox_rails_spec.rb
+++ spec/matchbox_rails_spec.rb
@@ -3,7 +3,5 @@ RSpec.describe MatchboxRails do
     expect(MatchboxRails::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # TODO: Write tests
 end
